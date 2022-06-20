a9fcc8e2ee627a87d5b6e5d8001f5f32314a45b1
brianmulier <brian.mulier.partner@decathlon.com>
2022-06-20 18:12:45 +0200

    FIX: generated id

--- src/main/java/com/oms/demo/tzatziki_quickstart/beans/dao/Booking.java
+++ src/main/java/com/oms/demo/tzatziki_quickstart/beans/dao/Booking.java
@@ -1,21 +1,22 @@
 package com.oms.demo.tzatziki_quickstart.beans.dao;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "booking")
 public class Booking {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String orderId;
     private String item;
     private Integer quantity;
